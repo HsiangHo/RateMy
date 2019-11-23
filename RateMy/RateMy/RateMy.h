762ba6bee6fc12cf938c29be580d105521a3a9d2
HsiangHo <ho_cheung@qq.com>
2019-11-23 15:47:10 +0800

    init project

---
+++ RateMy/RateMy/RateMy.h
@@ -0,0 +1,19 @@
+//
+//  RateMy.h
+//  RateMy
+//
+//  Created by Jovi on 11/23/19.
+//  Copyright © 2019 Jovi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RateMy.
+FOUNDATION_EXPORT double RateMyVersionNumber;
+
+//! Project version string for RateMy.
+FOUNDATION_EXPORT const unsigned char RateMyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RateMy/PublicHeader.h>
+
+
---
+++ RateMy/RateMyDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  RateMyDemo
+//
+//  Created by Jovi on 11/23/19.
+//  Copyright © 2019 Jovi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
